b2b5bd21add47dbc9577ee804ceba7d8fa63eb7f
bsodoge <kashhh4u@gmail.com>
2024-03-27 21:56:58 +0000

    Return country ip

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const app = express();
 
 app.get("/", (req, res) => {
 	let ip = req.headers['x-forwarded-for'] ||  req.socket.remoteAddress || null;
-	if(ip.startsWith("::ffff:")) ip = ip.substring(7);
-	res.send(geoip.lookup(ip));
+	if(ip.startsWith("::ffff:")) ip = ip.substring(7); 
+	const geo = geoip.lookup("94.2.58.89");
+	console.log(geo);
+	res.send(geo.country);
 })
 
 app.listen(3000);
