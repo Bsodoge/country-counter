03fe40d190b07c5ad793e63601027eafcaa93416
bsodoge <kashhh4u@gmail.com>
2024-04-01 17:19:09 +0100

    Group and add up countries

--- app.js
+++ app.js
@@ -13,14 +13,25 @@ app.get("/", async (req, res) => {
 		let ip = req.headers['x-forwarded-for'] ||  req.socket.remoteAddress || null;
 		if(!username || !username.length || !username.trim().length) throw new Error("Username not valid");
 		if(ip.startsWith("::ffff:")) ip = ip.substring(7); 
-		ip = "95.85.84.0";
+		ip = "175.45.176.15";
 		const geo = geoip.lookup(ip);
 		const countryCode = geo.country.toLowerCase();
-		const flag = await fs.readFile(`${__dirname}/country-flags/${countryCode}.svg`);
 		await sql`INSERT INTO country_information (country, ip) VALUES (${countryCode}, ${ip}) ON CONFLICT (ip) DO NOTHING;`
 		await sql`INSERT INTO user_information (username) VALUES (${username}) ON CONFLICT (username) DO NOTHING;`
-		await sql`INSERT INTO user_country_information (username, country, ip) VALUES (${username}, ${countryCode}, ${ip}) ON CONFLICT (ip) DO NOTHING;`
-		res.render("index", { countryCode, flag });
+		await sql`INSERT INTO user_country_information (username, ip) VALUES (${username}, ${ip}) ON CONFLICT (ip) DO NOTHING;`
+		const response = await sql`SELECT country_information.country FROM user_country_information RIGHT JOIN country_information ON user_country_information.ip = country_information.ip WHERE username=${username};`
+		const countries = response.map(obj => obj.country);
+		const countryCount = [];
+		for (const code of countries) {
+		  const country = countryCount.find(country => country.code === code);
+		  if(country){
+		  	country.count++; 
+ 			continue;
+		  }
+		  const flag = await fs.readFile(`${__dirname}/country-flags/${code}.svg`);
+		  countryCount.push({ code, flag, count: 1});
+		}
+		res.render("index", { countryCount });
 	}catch(e){
 		res.status(400).send({error: e.message})
 	}
