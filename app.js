32e50b13f15b4427be93ca5b2cd1c90ccd1713d2
bsodoge <kashhh4u@gmail.com>
2024-04-01 15:56:13 +0100

    Add records to tables

--- app.js
+++ app.js
@@ -13,13 +13,13 @@ app.get("/", async (req, res) => {
 		let ip = req.headers['x-forwarded-for'] ||  req.socket.remoteAddress || null;
 		if(!username || !username.length || !username.trim().length) throw new Error("Username not valid");
 		if(ip.startsWith("::ffff:")) ip = ip.substring(7); 
-		ip = "175.45.176.69";
+		ip = "95.85.84.0";
 		const geo = geoip.lookup(ip);
 		const countryCode = geo.country.toLowerCase();
 		const flag = await fs.readFile(`${__dirname}/country-flags/${countryCode}.svg`);
 		await sql`INSERT INTO country_information (country, ip) VALUES (${countryCode}, ${ip}) ON CONFLICT (ip) DO NOTHING;`
 		await sql`INSERT INTO user_information (username) VALUES (${username}) ON CONFLICT (username) DO NOTHING;`
-		await sql`INSERT INTO user_country_information (username, country) VALUES (${username}, ${countryCode}, ${ip}) ON CONFLICT (ip) DO NOTHING;`
+		await sql`INSERT INTO user_country_information (username, country, ip) VALUES (${username}, ${countryCode}, ${ip}) ON CONFLICT (ip) DO NOTHING;`
 		res.render("index", { countryCode, flag });
 	}catch(e){
 		res.status(400).send({error: e.message})
