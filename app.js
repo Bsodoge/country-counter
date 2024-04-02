dfcf5336cfde124b8116be620b37ab5c64e04698
bsodoge <kashhh4u@gmail.com>
2024-04-02 17:55:00 +0100

    Use req.ip

--- app.js
+++ app.js
@@ -13,10 +13,9 @@ app.use(express.static(path.join(__dirname, "public")))
 app.get("/", async (req, res) => {
 	try{
 		const { username } = req.query;
-		let ip = req.headers['x-forwarded-for'] ||  req.socket.remoteAddress || null;
+		let ip = req.ip ||  req.socket.remoteAddress || null;
 		if(!username || !username.length || !username.trim().length) throw new Error("Username not valid");
 		if(ip.startsWith("::ffff:")) ip = ip.substring(7); 
-		console.log(ip)
 		const geo = geoip.lookup(ip);
 		const countryCode = geo.country.toLowerCase();
 		await sql`INSERT INTO country_information (country, ip) VALUES (${countryCode}, ${ip}) ON CONFLICT (ip) DO NOTHING;`
