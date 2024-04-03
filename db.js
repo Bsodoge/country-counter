e3acfb04ba73abbabc0be92f20b08324329a481f
bsodoge <kashhh4u@gmail.com>
2024-04-03 14:23:49 +0100

    Remove log

--- db.js
+++ db.js
@@ -2,7 +2,6 @@ const postgres = require("postgres");
 require('dotenv').config();
 
 const connectionString = process.env.DATABASE_URL;
-console.log(process.env.DATABASE_URL)
 const sql = postgres(connectionString);
 
 module.exports = sql;
